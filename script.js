8ceaaf8b3ac8e0d260b13f3fea7128345fb9bf75
sereyoudomlong <sereyoudom78@gmail.com>
2024-02-10 10:01:47 +1100

    Added playRound to evaluate choice

--- script.js
+++ script.js
@@ -3,14 +3,10 @@
  * @returns the randomised choice
  */
 function getComputerChoice() {
-  choices = {
-    Rock: "Rock",
-    Paper: "Paper",
-    Scissor: "Scissor",
-  };
+  choices = ["Rock", "Scissor", "Paper"];
 
-  var rand = Math.floor(Math.random() * Object.keys(choices).length);
-  var computerChoice = choices[Object.keys(choices)[rand]];
+  var rand = Math.floor(Math.random() * choices.length);
+  var computerChoice = choices[rand];
 
   return computerChoice;
 }
@@ -20,4 +16,30 @@ function getComputerChoice() {
  * @param {string} playerSelection
  * @param {string} computerSelection
  */
-function playRound(playerSelection, computerSelection) {}
+function playRound(playerSelection, computerSelection) {
+  whatBeats = {
+    scissor: "rock",
+    rock: "paper",
+    paper: "scissor",
+  };
+
+  if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+    return "Draw";
+  }
+
+  if (
+    whatBeats[playerSelection.toLowerCase()] == computerSelection.toLowerCase()
+  ) {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  }
+
+  if (
+    whatBeats[computerSelection.toLowerCase()] == playerSelection.toLowerCase()
+  ) {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
